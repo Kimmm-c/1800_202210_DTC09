5b8f7ab94c6eba81bd973bf9a8c553e6fa9d29ef
Oscar Zhu <oscar@yvrmail.net>
2022-03-02 18:51:12 -0800

    skeleton adjustments

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -3,7 +3,8 @@
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
 function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
+    console.log($('#navbarPlaceholder').load('./text/navBar.html'));
+    console.log($('#footerNavBar').load('./text/footerNavBar.html'));
     console.log($('#footerPlaceholder').load('./text/footer.html'));
 }
 loadSkeleton();  //invoke the function
