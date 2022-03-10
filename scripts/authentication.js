351b25e6caa1dbf4574f0ac5ba51facbc650f8dc
Kim Chung <kchung68@my.bcit.ca>
2022-03-09 18:02:46 -0800

    add user comment and user draft to database - forum

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -49,3 +49,20 @@ var uiConfig = {
 };
 
 ui.start('#firebaseui-auth-container', uiConfig);
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCFI7f6upIpgTyBbMLqeyiWOUrG5GNvzLE",
+  authDomain: "friend-comp-1800.firebaseapp.com",
+  projectId: "friend-comp-1800",
+  storageBucket: "friend-comp-1800.appspot.com",
+  messagingSenderId: "316008911447",
+  appId: "1:316008911447:web:1bc58999f78082b349c912",
+  measurementId: "G-30X5HEPPK8"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
