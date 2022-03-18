5782517e91b4f6ea77dfaf8d0ee122fba517f6c6
Oscar Zhu <oscar@yvrmail.net>
2022-03-18 04:17:40 -0700

    redone the account page

--- scripts/account.js
+++ scripts/account.js
@@ -3,11 +3,21 @@ function populateAccount() {
         if (user) {
             currentUser = db.collection("user").doc(user.uid);
             currentUser.get().then(userDoc => {
-                $('#accountName').text(userDoc.data().name)
-                $('#accountEmail').text(userDoc.data().email)
+                $('#accountName').attr("value", userDoc.data().name)
+                $('#accountEmail').attr("value", userDoc.data().email)
+                $(`#accountGender[value="${userDoc.data().gender}"]`).prop('checked', 'checked')
+                $('#birthday').attr('value', userDoc.data().birthdate)
+                $(`option[value="${userDoc.data().country}"]`).prop('selected', 'selected')
             })
         }
     })
 }
 
+var loadFile = function (event) {
+    var image = document.getElementById("output");
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
+
+  
 populateAccount();
+
