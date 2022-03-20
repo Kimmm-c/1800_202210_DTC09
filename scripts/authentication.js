9a820004de7c37cbb4d16c25ad3b00058194937d
Oscar Zhu <oscar@yvrmail.net>
2022-03-20 02:12:46 -0700

    added FE and BE for additional user info feature

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,20 +1,18 @@
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
 var uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle. 
-      var user = authResult.user;                            // get the user object from the Firebase authentication database
-      if (authResult.additionalUserInfo.isNewUser) {         //if new user
-        db.collection("user").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
-          name: user.displayName,                    //"users" collection
-          email: user.email                          //with authenticated user's ID (user.uid)
+      var user = authResult.user;                            
+      if (authResult.additionalUserInfo.isNewUser) {         
+        db.collection("user").doc(user.uid).set({        
+          name: user.displayName,                   
+          email: user.email                            
         }).then(function () {
           console.log("New user added to firestore");
-          window.location.assign("main.html");       //re-direct to main.html after signup
+          setUserID(user.uid);
+          hideFireAuth();
+          //window.location.assign("login2.html");      
         })
           .catch(function (error) {
             console.log("Error adding new user: " + error);
@@ -26,26 +24,45 @@ var uiConfig = {
     },
   },
   uiShown: function () {
-    // The widget is rendered.
-    // Hide the loader.
     document.getElementById('loader').style.display = 'none';
   },
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
   signInSuccessUrl: 'main.html',
   signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
   ],
   // Terms of service url.
   tosUrl: '<your-tos-url>',
   // Privacy policy url.
   privacyPolicyUrl: '<your-privacy-policy-url>'
 };
+
+function hideFireAuth() {
+    $('#firebaseui-auth-container').toggle()
+    $('#signup-form').toggle()
+}
+
+function setUserID(element) {
+  localStorage.setItem('uid', element)
+}
+
+function saveInfo() {
+    let uid = localStorage.getItem('uid');
+    db.collection("user").doc(uid).update({   
+        birthdate: $('#birthday').val(),
+        gender: $('input[name="gender"]:checked').val(),
+        country:  $(`select[name="country"]:selected`).text(),
+        profilePic: null,
+    })
+    .then(() => {
+      console.log('Successfully added additional info to firebase');
+      window.location.assign('main.html');
+    })
+    .catch(e => console.log(e))
+}
+
+
+
 
 ui.start('#firebaseui-auth-container', uiConfig);
