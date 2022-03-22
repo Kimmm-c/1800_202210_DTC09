e7dcff36ac8d6fbc343aec117f3f20d1f54db9a1
Kim Chung <kchung68@my.bcit.ca>
2022-03-21 22:25:02 -0700

    add journal and thread forms for frontend, save/post data to/from database

--- scripts/forum.js
+++ scripts/forum.js
@@ -4,27 +4,14 @@ db.collection("thread").get().then((data) => {
     });
 })
 
-function renderThread(data){
+function renderThread(data) {
     //console.log(data);
     //console.log(data.title);
     //console.log(data.details);
     $(".thread_render").append(`<div class="single_thread">
     <h5><b>${data.title}</b></h5>
-    <p>${data.details}</p></div>`)
+    <p>${data.content}</p></div>`)
 }
-function addThread() {
-    max = 5;
-    //define a variable for the collection you want to create in Firestore to populate data
-    var hikesRef = db.collection("thread");
-    for (i = 1; i <= max; i++) {
-        hikesRef.add({ //add to database, autogen ID
-            code: "id" + i,
-            title: "Title" + i,
-            details: "Testing" + i
-        })
-    }
-}
-
 
 function writeReview() {
     let Description = document.getElementById("description").value;
@@ -53,91 +40,73 @@ function writeReview() {
 }
 
 function saveJournal() {
-    let title = document.getElementById("journal_title").value;
-    let content = document.getElementById("journal_content").value;
-    console.log(title);
-    console.log(content);
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
-            var currentUser = db.collection("user").doc(user.uid)
-            var userID = user.uid;
-            //get the document for current user.
-            currentUser.get()
-                .then(userDoc => {
-                    var userEmail = userDoc.data().email;
-                    db.collection("journals").add({
-                        title: title,
-                        content: content,
-                        userid: userID
-                    }).then(() => {
-                        //window.location.href = "http://127.0.0.1:5500/html/forum/index.html";
-                        console.log("journal added into database")
-                    })
-                })
-        } else {
+            db.collection("journals").add({
+                title: $("#journal_title").val(),
+                content: $("#journal_content").val(),
+                userID: user.uid
+            })
+            console.log("data added");
+            $("#journal_title").val('');
+            $("#journal_content").val('');
+        }else{
             console.log("user not signed in")
         }
-    });
+    })
 }
 
 function saveThread() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
-            let title = document.getElementById("thread_title").value;
-            let content = document.getElementById("thread_content").value;
-            console.log(title);
-            console.log(content);
-            var currentUser = db.collection("user").doc(user.uid);
-            var userID = user.uid;
-            currentUser.get()
-                .then(userDoc => {
-                    var userEmail = userDoc.data().email;
-                    db.collection("thread").add({
-                        title: title,
-                        content: content,
-                        userid: userID
-                    }).then(() => {
-                        //window.location.href = "http://127.0.0.1:5500/html/forum/index.html";
-                        console.log("thread added into database")
-                    })
-                })
-        } else {
+            db.collection("thread").add({
+                title: $("#thread_title").val(),
+                content: $("#thread_content").val(),
+                userID: user.uid
+            })
+            console.log("data added");
+            $("#thread_title").val('');
+            $("#thread_content").val('');
+        }else{
             console.log("user not signed in")
         }
-    });
-}
+    })}
 
 function display_thread_form() {
-    $("#thread").empty();
-    $("#journal").empty();
-    $("#thread").append(`<form>
+            $("#thread").empty();
+            $("#journal").empty();
+            $("#write_journal").css("text-decoration", "none");
+            $(this).css("text-decoration", "underline");
+            $("#thread").append(`
     <label for="title"><b>Title:</b></label><br>
     <input type="text" id="thread_title"><br>
     <label for="content"><b>Content:</b></label><br>
     <textarea id="thread_content" rows="5" cols="50"></textarea><br>
     <button class="cancel_button">Cancel</button>
-    <button id="thread_submit_button" onclick=saveThread()>Post</button></form>`)
-    
-}
+    <button id="thread_submit_button" onclick=saveThread()>Post</button>`)
+
+        }
 
 function display_journal_form() {
-    $("#thread").empty();
-    $("#journal").empty();
-    $("#journal").append(`<form>
+            $("#thread").empty();
+            $("#journal").empty();
+            $("#start_thread").css("text-decoration", "none");
+            $(this).css("text-decoration", "underline");
+            $("#journal").append(`
     <label for="title"><b>Title:</b></label><br>
     <input type="text" id="journal_title"><br>
     <label for="content"><b>Content:</b></label><br>
     <textarea id="journal_content" rows="5" cols="50"></textarea><br>
     <button class="cancel_button">Cancel</button>
-    <button id="journal_submit_button" onclick=saveJournal()>Save</button></form>`)
+    <button id="journal_submit_button" onclick=saveJournal()>Save</button>`)
 
-}
+        }
 function setup() {
-    $("#start_thread").click(display_thread_form);
-    $("#write_journal").click(display_journal_form);
-    //$('#journal_submit_button').click(saveJournal);
-    //$("#thread_submit_button").click(saveThread);
-}
+            $("#start_thread").click(display_thread_form);
+            $("#write_journal").click(display_journal_form);
+            //$('#journal_submit_button').click(saveJournal);
+            //$("#thread_submit_button").click(saveThread);
+        }
 $(document).ready(setup);
 
 
