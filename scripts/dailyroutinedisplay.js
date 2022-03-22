d5a306f24e0121925d93b915b878228fba355388
JoushuaDelaCruz <97211895+JoushuaDelaCruz@users.noreply.github.com>
2022-03-21 20:10:05 -0700

    Modifying moodtracker

---
+++ scripts/dailyroutinedisplay.js
@@ -0,0 +1,27 @@
+function displayCards() {
+    let taskcardTemplate = document.getElementById("TasksCardsTemplate");
+    let TasksCardsGroup = document.getElementById("TasksCardsGroups")
+
+    db.collection("MockSchedule").get()
+        .then(alltasks => {
+            alltasks.forEach(doc => { //iterate thru each doc
+                var status = doc.data().done;   // get value of the "done" key
+                var task = doc.data().Task;   // get value of the "task" key
+                var rating = doc.data().rating; // get value of the "rating" key
+                var activity = doc.data().Type; // get value of the "Type" key
+                var icon = doc.data().icon;   // get value of the "icon" key
+                
+                let newtaskcard = taskcardTemplate.content.cloneNode(true);
+                //update title and text and image
+                newtaskcard.querySelector('.card-task').innerHTML = task;
+                newtaskcard.querySelector('.icon-goes-here').innerHTML = icon;
+                newtaskcard.querySelector('.activity-type').innerHTML = activity;
+                newtaskcard.querySelector('.activity-status').innerHTML = status;
+                newtaskcard.querySelector('.activity-mood').innerHTML = rating;
+                
+                TasksCardsGroup.appendChild(taskcardTemplate)
+            })
+        })
+}
+
+displayCards();
