03112396818a2a28c9f165cc84975fa73ddef1e3
Kim Chung <kchung68@my.bcit.ca>
2022-03-21 20:47:37 -0700

    populate thread and display in forum

--- scripts/forum.js
+++ scripts/forum.js
@@ -1,3 +1,17 @@
+db.collection("thread").get().then((data) => {
+    data.docs.forEach(doc => {
+        renderThread(doc.data());
+    });
+})
+
+function renderThread(data){
+    //console.log(data);
+    //console.log(data.title);
+    //console.log(data.details);
+    $(".thread_render").append(`<div class="single_thread">
+    <h5><b>${data.title}</b></h5>
+    <p>${data.details}</p></div>`)
+}
 function addThread() {
     max = 5;
     //define a variable for the collection you want to create in Firestore to populate data
@@ -8,7 +22,7 @@ function addThread() {
             title: "Title" + i,
             details: "Testing" + i
         })
-   }
+    }
 }
 
 
@@ -39,9 +53,10 @@ function writeReview() {
 }
 
 function saveJournal() {
-    let title = document.getElementById("title").value;
-    let Description = document.getElementById("description").value;
-    //console.log(Title, Level, Season, Description, Flooded, Scrambled);
+    let title = document.getElementById("journal_title").value;
+    let content = document.getElementById("journal_content").value;
+    console.log(title);
+    console.log(content);
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
             var currentUser = db.collection("user").doc(user.uid)
@@ -52,18 +67,77 @@ function saveJournal() {
                     var userEmail = userDoc.data().email;
                     db.collection("journals").add({
                         title: title,
-                        details: Description,
-
+                        content: content,
+                        userid: userID
                     }).then(() => {
-                        window.location.href = "forum.html";
+                        //window.location.href = "http://127.0.0.1:5500/html/forum/index.html";
+                        console.log("journal added into database")
                     })
                 })
-
         } else {
-            // No user is signed in.
+            console.log("user not signed in")
         }
     });
+}
+
+function saveThread() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            let title = document.getElementById("thread_title").value;
+            let content = document.getElementById("thread_content").value;
+            console.log(title);
+            console.log(content);
+            var currentUser = db.collection("user").doc(user.uid);
+            var userID = user.uid;
+            currentUser.get()
+                .then(userDoc => {
+                    var userEmail = userDoc.data().email;
+                    db.collection("thread").add({
+                        title: title,
+                        content: content,
+                        userid: userID
+                    }).then(() => {
+                        //window.location.href = "http://127.0.0.1:5500/html/forum/index.html";
+                        console.log("thread added into database")
+                    })
+                })
+        } else {
+            console.log("user not signed in")
+        }
+    });
+}
+
+function display_thread_form() {
+    $("#thread").empty();
+    $("#journal").empty();
+    $("#thread").append(`<form>
+    <label for="title"><b>Title:</b></label><br>
+    <input type="text" id="thread_title"><br>
+    <label for="content"><b>Content:</b></label><br>
+    <textarea id="thread_content" rows="5" cols="50"></textarea><br>
+    <button class="cancel_button">Cancel</button>
+    <button id="thread_submit_button" onclick=saveThread()>Post</button></form>`)
+    
+}
+
+function display_journal_form() {
+    $("#thread").empty();
+    $("#journal").empty();
+    $("#journal").append(`<form>
+    <label for="title"><b>Title:</b></label><br>
+    <input type="text" id="journal_title"><br>
+    <label for="content"><b>Content:</b></label><br>
+    <textarea id="journal_content" rows="5" cols="50"></textarea><br>
+    <button class="cancel_button">Cancel</button>
+    <button id="journal_submit_button" onclick=saveJournal()>Save</button></form>`)
 
 }
+function setup() {
+    $("#start_thread").click(display_thread_form);
+    $("#write_journal").click(display_journal_form);
+    //$('#journal_submit_button').click(saveJournal);
+    //$("#thread_submit_button").click(saveThread);
+}
+$(document).ready(setup);
 
 
