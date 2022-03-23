d9538cd2e91669a522436bd267ac107c6d8835da
JoushuaDelaCruz <97211895+JoushuaDelaCruz@users.noreply.github.com>
2022-03-23 10:54:00 -0700

    Done CSS for moodtracker

--- scripts/dailyroutinedisplay.js
+++ scripts/dailyroutinedisplay.js
@@ -6,9 +6,9 @@ function displayCards() {
         .then(alltasks => {
             alltasks.forEach(doc => { //iterate thru each doc
                 var status = doc.data().done;   // get value of the "done" key
-                var task = doc.data().Task;   // get value of the "task" key
+                var task = doc.data().task;   // get value of the "task" key
                 var rating = doc.data().rating; // get value of the "rating" key
-                var activity = doc.data().Type; // get value of the "Type" key
+                var activity = doc.data().type; // get value of the "Type" key
                 var icon = doc.data().icon;   // get value of the "icon" key
               
                 let newtaskcard = taskcardTemplate.content.cloneNode(true);
