ef4051ae99981f7fe4d5ad1455781b56b9993b51
Kim Chung <kchung68@my.bcit.ca>
2022-03-23 21:42:56 -0700

    add delete post function

--- scripts/forum.js
+++ scripts/forum.js
@@ -1,18 +1,30 @@
 db.collection("thread").get().then((data) => {
     data.docs.forEach(doc => {
-        renderThread(doc.data());
+        renderThread(doc.data(), doc.id);
     });
 })
 
-function renderThread(data) {
+function renderThread(data, data_id) {
     //console.log(data);
     //console.log(data.title);
     //console.log(data.details);
-    $(".thread_render").append(`<div class="single_thread">
-    <h5><b>${data.title}</b></h5>
-    <p>${data.content}</p></div>`)
-}
+    firebase.auth().onAuthStateChanged(user =>{
+    //console.log(user.uid);
+    //console.log(data.userID);
+    if (user.uid == data.userID) {
+        $(".thread_render").append(`<div class="single_thread" id=${data_id}>
+        <h5><b>${data.title}</b></h5>
+        <button class="thread_buttons edit_post">Edit post</button><button class="thread_buttons delete_post">Delete post</button>
+        <p>${data.content}</p></div>`)
+    } else {
+        $(".thread_render").append(`<div class="single_thread">
+        <h5><b>${data.title}</b></h5>
+        <button class="thread_buttons report_post">Report</button>
+        <p>${data.content}</p></div>`)
+    }
+})}
 
+//Last update: ${data.last_updated.toDate()}
 function writeReview() {
     let Description = document.getElementById("description").value;
     //console.log(Title, Level, Season, Description, Flooded, Scrambled);
@@ -45,7 +57,8 @@ function saveJournal() {
             db.collection("journals").add({
                 title: $("#journal_title").val(),
                 content: $("#journal_content").val(),
-                userID: user.uid
+                userID: user.uid,
+                last_updated: firebase.firestore.FieldValue.serverTimestamp()
             })
             console.log("data added");
             $("#journal_title").val('');
@@ -62,7 +75,8 @@ function saveThread() {
             db.collection("thread").add({
                 title: $("#thread_title").val(),
                 content: $("#thread_content").val(),
-                userID: user.uid
+                userID: user.uid,
+                last_updated: firebase.firestore.FieldValue.serverTimestamp()
             })
             console.log("data added");
             $("#thread_title").val('');
@@ -102,12 +116,20 @@ function display_journal_form() {
     <button id="journal_submit_button" onclick=saveJournal()>Save</button>`)
 
 }
+
+function deleteThread(){
+    threadID=$(this).parent().attr("id");
+    db.collection("thread").doc(threadID).delete();
+}
+
 function setup() {
     $("#start_thread").click(display_thread_form);
     $("#write_journal").click(display_journal_form);
-    $("body").on('click', ".cancel_button", () => { 
-        window.location.href = "http://127.0.0.1:5500/html/forum/index.html";
+    $("body").on('click', ".cancel_button", () => {
+        window.location.href = "index.html";
     })
+    $("#delete_post").click(deleteThread);
+    $("body").on("click",".thread_buttons",deleteThread);
     //$('#journal_submit_button').click(saveJournal);
     //$("#thread_submit_button").click(saveThread);
 }
